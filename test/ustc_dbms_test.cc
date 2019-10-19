4eebb2193ee2ad1546588aded3984f03e6eb9dee
zonghua <zonghuaxiansheng@outlook.com>
2019-10-19 11:35:05 +0800

    Update test and make file.

---
+++ test/ustc_dbms_test.cc
@@ -0,0 +1,12 @@
+#include "buffer_manager.h"
+#include "data_storage_manager.h"
+#include <iostream>
+
+int main(void) {
+
+  std::cout << "***********************USTC DBMS TEST***********************" << std::endl;
+  // ...
+  std::cout << "***********************USTC DBMS TEST***********************" << std::endl;
+  
+  return 0;
+}
